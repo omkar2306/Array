3731213dd6845376b093a13e77996815a7c0f439
omkar2306 <61349460+omkar2306@users.noreply.github.com>
2021-07-22 01:01:54 -0700

    Add files via upload

---
+++ array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a[5],i;
+printf("please take the element of array\n");
+for(i=0;i<5;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("print the elements of array\n");
+for(i=0;i<5;i++)
+{
+printf("%d\n",a[i]);
+}
+}
